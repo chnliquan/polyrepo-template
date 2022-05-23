3a0dae9b99830adada724ae98b8b1b366084f004
chnliquan <chnliquan@163.com>
2022-05-23 18:47:27 +0800

    chore: update template

--- node/jest.config.js
+++ node/jest.config.js
@@ -23,9 +23,6 @@ module.exports = {
   collectCoverageFrom: ['/src/**/*.ts'],
   watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  moduleNameMapper: {
-    '^@<%= locals.shortName %>/(.*?)$': '<rootDir>/packages/$1/src',
-  },
   testMatch: ['<rootDir>/__tests__/**/*spec.[jt]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/examples/__tests__'],
 }
