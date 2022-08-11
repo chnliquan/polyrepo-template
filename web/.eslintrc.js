1155d49a9fca3a4c87ce7748b2c8052740af2b3f
chnliquan <chnliquan@163.com>
2022-08-11 13:06:11 +0800

    chore: update template

---
+++ web/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react-hooks', 'dirs'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: ['memberLike', 'property', 'method'],
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'require',
+        modifiers: ['private'],
+      },
+    ],
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'dirs/dirnames': ['error', { pattern: '^([a-z0-9\\-]+)|__tests__$' }],
+    'dirs/filenames': [
+      'error',
+      {
+        '**/*.md/*': '.*',
+        '**/*': '^[a-z0-9\\-\\.]+$',
+      },
+    ],
+    'import/order': 'off',
+  },
+}
